7be9a706cf93cd0b3345ec4bbf590803c8c62c82
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-23 19:37:27 +0000

    String

---
+++ Basic/7_String.js
@@ -0,0 +1,14 @@
+const name="Rajat"
+let age=24;
+// console.log("Myname is " +name + " and My age is" + age); //old generation
+console.log(`My name is ${name} and my age is ${age}`); // letest
+
+const newName=new String('Rajat')// another type to write string
+console.log(newName);
+console.log(newName[2]); // index position
+
+console.log(newName.length);
+console.log(newName.charAt(2));
+console.log(newName.indexOf("a"));
+
+// 11 num vidio

0f14957d2ce4e8d473818fe52f4c4ccc1d6acd5b
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-23 18:33:13 +0000

    Operation

---
+++ Basic/4_Operation.js
@@ -0,0 +1,37 @@
+// *********** Operation *****************
+let value=20;
+let negValue=-value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="rajat";
+let str2=" Tanu"
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+2+'3');//execute L to R default
+
+console.log(true);
+console.log(+true);// output 1 bcos of this is boolean
+console.log(+""); // 0 empty means false
+
+// *********prefix***********
+// let num=10;
+// ++num;
+// console.log(num);
+// ********post fix************
+let num=10;
+num++;
+console.log(num);
+
+
+
+
+

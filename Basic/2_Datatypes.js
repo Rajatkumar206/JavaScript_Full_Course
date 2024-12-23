ccc5928eba66a850dfae8e28f44e2b28d2e07072
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-23 17:01:58 +0000

    Datatypes

---
+++ Basic/2_Datatypes.js
@@ -0,0 +1,19 @@
+// "use strict"; // treat all js code as newer version
+// alert() // use only browser
+let name="Rajat";
+let age = 24;
+let isLoggedin=true;
+
+// =====Primitive Datatypes===========
+
+// number=>2 to the power 53
+// bigInt
+//string=>  ""
+// Boolean = true/ false
+//null (standlon Value)= empty value => it is a object
+// undefind= not asigned value
+//symbol => Unique
+
+// =========== Non Primitive Datatypes============
+// Object
+console.log(typeof null);
--- Basic/Basic.js
+++ Basic/Basic.js
@@ -1,2 +1,15 @@
 // Print Code
-// console.log("rr");
+// console.log("Rajat");
+const accountId=1234;
+let email="xyz@gmail.com";
+var name="rajat";
+city="jaipur";
+// accountId=12345;-----after declare same name const then we can not declare same name const variable
+console.log(accountId);
+console.table([accountId, email, name, city]);
+// not prefer use var becos of issue in block scope and functional scope
+
+
+
+
+

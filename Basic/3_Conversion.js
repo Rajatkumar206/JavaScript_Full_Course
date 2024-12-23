43743dced127497d9468482aff7b4e93ea4639cc
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-23 17:23:29 +0000

    Conversion

---
+++ Basic/3_Conversion.js
@@ -0,0 +1,17 @@
+let score=56;
+//let score=null; output = 0
+// const {score}=req.body
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInnumber=String(score);// 100% sure convert str to num 
+
+console.log(typeof valueInnumber);
+console.log(valueInnumber);// output NaN
+
+// string=> NaN
+// Number=>String
+// null= 0
+//undefind= undefind
+// true  =1
+// false=0

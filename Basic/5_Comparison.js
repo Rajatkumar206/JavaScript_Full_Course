fec87adcebf99136482bbed2334b3d9315358e7a
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-23 19:06:22 +0000

    Datatypesummery

---
+++ Basic/5_Comparison.js
@@ -0,0 +1,15 @@
+// this operator exwcute in boolean values
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2">1);
+
+// console.log(null==0);false
+// console.log(null>=0); false
+// console.log(null<=0); // out put true
+
+// === strict chaek it is also cheak data types also
+
+
---
+++ Basic/6_Datatypes_summery.js
@@ -0,0 +1,34 @@
+// =====Primitive Datatypes===========
+// 
+//1. number=>2 to the power 53
+//2. bigInt
+//3. string=>  ""
+//4. Boolean = true/ false
+//5. null (standlon Value)= empty value => it is a object
+//6. undefind= not asigned value
+//7.symbol => Unique
+let id=Symbol('123');
+let id2=Symbol('123')
+console.log(id===id2);
+
+let biginte=122222n;
+console.log(typeof(biginte));
+
+
+
+// =========== Non Primitive Datatypes/ Reference type============
+//1. Object
+// 2. Array
+// 3. Functions
+
+const frnd=["Raju", "raj", "Farhan", "Tanu"]// array example
+const prof={
+    name:"raj",
+    age:24,
+    gender:male
+}// Object Example
+
+// variable type function
+const Idea = function (){
+    console.log("Hello World");
+}

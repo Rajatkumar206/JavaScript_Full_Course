7c2e05f9d20cd788c0478abe3c32a392852d2fac
RAJAT KUMAR DAS <drajatkumar206@gmail.com>
2024-12-26 16:05:05 +0000

    sss

--- Basic/10_Array.js
+++ Basic/10_Array.js
@@ -1,6 +1,6 @@
-let arr=['Rajat','Rajnu','kunal','tanu', 'tanu']
-let arrnum=[1,2,3,4,5,6,7,8,9,0]
-console.log(arr)
+let arr=["Rajat","Tanu", "Kunal", "saroj"]
+// let arrnum=[1,2,3,4,5,6,7,8,9,0]
+// console.log(arr)
 
 // array Methods
 
@@ -8,13 +8,24 @@ console.log(arr)
 // arr.pop()// remove last value
 // arr.unshift(1)// insert value 1st position
 // arr.shift()// remove value 1st postion
-console.log(arr.includes(3))// find value
+// console.log(arr.includes(3))// find value
 
-let arr1=arr.join() // convert to string
+// let arr1=arr.join() // convert to string
 
 
-console.log("rajat " ,arr)
+// console.log("rajat " ,arr)
+// const s=arrnum.slice(1, 4);
+// console.log(s)
+
+// const s1=arrnum.splice(1, 4)
+// console.log(s1)
+
 
 //slice and splice
 // *************************************************
-
+const heros=["shaktiman", "spiderman", "Batman"]
+arr.push(heros)
+// console.log(arr)
+// console.log(arr[3][1])
+arr.concat(heros)
+console.log(arr)
